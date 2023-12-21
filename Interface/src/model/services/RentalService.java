74e4d39821ac7330272b1a97974c3ad4486803d9
Jeffleyg <jefleygarcon007@gmail.com>
2023-12-21 20:48:42 -0300

    meu commit

---
+++ Interface/src/model/services/RentalService.java
@@ -0,0 +1,47 @@
+package model.services;
+
+import java.time.Duration;
+
+import model.entities.CarRental;
+import model.entities.Invoice;
+
+public class RentalService {
+	
+	private Double pricePerHour;
+	
+	private Double pricePerDay;
+	
+	private BrazilTaxService taxService;
+
+	public RentalService(Double pricePerHour, Double pricePerDay, BrazilTaxService taxService) {
+		this.pricePerHour = pricePerHour;
+		this.pricePerDay = pricePerDay;
+		this.taxService = taxService;
+	}
+	
+	public void processInvoice(CarRental carRental) {
+		
+		double minutes = Duration.between(carRental.getStart(), carRental.getFinish()).toMinutes();
+		double hours = minutes / 60.0;
+		
+		double basicPayment;
+		if(hours <= 12.0) {
+			basicPayment = pricePerHour * Math.ceil(hours); // mah.ceil é para aredondar para cima;
+		}
+		else {
+			basicPayment = pricePerDay * Math.ceil(hours / 60.0);
+		}
+	
+		
+		double tax = taxService.tax(basicPayment);	
+		carRental.setInvoice(new Invoice(basicPayment, tax));
+		
+		
+	}
+	
+	
+	
+	
+	
+
+}
