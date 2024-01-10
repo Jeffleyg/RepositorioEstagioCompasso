fb9cc668f9bcdbba1ab931fded4109022e12e48e
Jeffleyg <jeffleygarcon007@gmail.com>
2024-01-10 15:47:06 -0300

    commit

--- demo-park-api/demo-park-api/src/main/java/com/mballem/demoparkapi/repository/UsuarioRepository.java
+++ demo-park-api/demo-park-api/src/main/java/com/mballem/demoparkapi/repository/UsuarioRepository.java
@@ -2,7 +2,8 @@ package com.mballem.demoparkapi.repository;
 
 import com.mballem.demoparkapi.entity.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+public interface UsuarioRepository extends JpaRepository<Usuario, Long>, JpaSpecificationExecutor<Usuario> {
 
 }
--- demo-park-api/demo-park-api/src/main/java/com/mballem/demoparkapi/web/controller/UsuarioController.java
+++ demo-park-api/demo-park-api/src/main/java/com/mballem/demoparkapi/web/controller/UsuarioController.java
@@ -7,9 +7,11 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RequiredArgsConstructor
 @RestController
-@RequestMapping("api/v1/usuarios/1")
+@RequestMapping("api/v2/usuarios")
 public class UsuarioController {
 
     private final UsuarioService usuarioService;
@@ -23,6 +25,19 @@ public class UsuarioController {
     @GetMapping("/{id}")
     public ResponseEntity<Usuario> getById(@PathVariable Long id){
         Usuario user = usuarioService.buscarPorId(id);
-        return  ResponseEntity.ok(user);
+        return ResponseEntity.ok(user);
     }
+
+    @PatchMapping ("/{id}")
+    public ResponseEntity<Usuario> updatePassword(@PathVariable Long id, @RequestBody Usuario usuario){
+        Usuario user = usuarioService.editarSenha(id, usuario.getPassword());
+        return ResponseEntity.ok(user);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Usuario>> getAll(){
+        List<Usuario> users = usuarioService.buscarTodos();
+        return ResponseEntity.ok(users);
+    }
+
 }
